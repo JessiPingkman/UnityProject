bccd246820bc75a82e26e173babd12d755935a50
JessiPingkman <jokerxxx99291488@gmail.com>
2018-06-15 16:58:04 +0600

    Bug fixed and add new components and text

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,10 +10,10 @@ public class PlayerMovement : MonoBehaviour {
         rigidBody.AddForce(0, 0, forwardForse * Time.deltaTime);
 
         if (Input.GetKey("d")) {
-            rigidBody.AddForce(sidewayForse * Time.deltaTime, 0, 0);
+            rigidBody.AddForce(sidewayForse * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
         if (Input.GetKey("a")) {
-            rigidBody.AddForce(-sidewayForse * Time.deltaTime, 0, 0);
+            rigidBody.AddForce(-sidewayForse * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
     }
 }
